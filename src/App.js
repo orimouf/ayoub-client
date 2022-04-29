24f27c7c5aecd5c6ad21337f7a3e3f46603f00c1
unknown <orim.ouf@gmail.com>
2022-04-29 16:30:51 +0100

    heroku react app

--- src/App.js
+++ src/App.js
@@ -1,75 +1,49 @@
-// import axios from "axios";
-// import './css/App.css';
-// import Header from './templates/Header';
-// import MainBanner from './templates/MainBanner';
-// import Home from './templates/Home';
-// import Shop from './pages/Shop';
-// import Footer from './templates/Footer';
-// import ShippingInfo from './templates/ShippingInfo'
-// import NewsLetter from './templates/NewsLetter'
-// import Copyright from './templates/Copyright'
-// import BestSelling from './pages/BestSelling'
-// import Single from './pages/Single';
-// import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="main-sec">
-//         <Header />
-//         <MainBanner />
-//       </div>
-//       <Switch>
-//           <Route path="/" exact><Shop /><BestSelling /></Route>
-//           <Route path="/single" exact><Single /></Route>
-//       </Switch>
-
-//       <ShippingInfo />
-//       <NewsLetter />
-//       <Footer />
-//       <Copyright />
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
-import React from "react";
 import axios from "axios";
-import "./styles.css";
+import './css/App.css';
+import Header from './templates/Header';
+import MainBanner from './templates/MainBanner';
+import Home from './templates/Home';
+import Shop from './pages/Shop';
+import Footer from './templates/Footer';
+import ShippingInfo from './templates/ShippingInfo'
+import NewsLetter from './templates/NewsLetter'
+import Copyright from './templates/Copyright'
+import BestSelling from './pages/BestSelling'
+import Single from './pages/Single';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
 
-export default class App extends React.Component {
-  state = {
-    users: [],
-  };
-  componentDidMount() {
-    axios.get("/users.json").then((response) => {
-      this.setState({ users: response.data });
-    });
+function App() {
+  const [apiResponse, setApiResponse]= useState([]);
+
+  const callAPI = (order) => {
+    fetch("https://backend-node.azurewebsites.net/" + order, { mode: 'no-cors'})
+    .then(res => res.text())
+    .then(res => setApiResponse(res))
   }
 
-  render() {
-    const { users } = this.state;
-    return (
-      <div>
-        <ul className="users">
-          {users.map((user) => (
-            <li className="user">
-              <p>
-                <strong>Name:</strong> {user.name}
-              </p>
-              <p>
-                <strong>Email:</strong> {user.email}
-              </p>
-              <p>
-                <strong>City:</strong> {user.address.city}
-              </p>
-            </li>
-          ))}
-        </ul>
+  useEffect(() => {
+    callAPI('users')
+  });
+
+  return (
+    <Router>
+      <div className="main-sec">
+        <Header />
+        <MainBanner />
+        <p>{apiResponse}</p>
       </div>
-    );
-  }
+      <Switch>
+          <Route path="/" exact><Shop /><BestSelling /></Route>
+          <Route path="/single" exact><Single /></Route>
+      </Switch>
+
+      <ShippingInfo />
+      <NewsLetter />
+      <Footer />
+      <Copyright />
+    </Router>
+  );
 }
+
+export default App;
